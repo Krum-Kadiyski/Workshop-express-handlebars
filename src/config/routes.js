cd8b4a24258e04ab6093148a2c2d343ad8e142b1
Krum Kadiyski <krum.kadiyski@gmail.com>
2024-06-03 11:36:41 +0300

    add more views

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,18 @@
 const { Router } = require('express');
-const { homeController } = require('../controllers/homeController');
+const { home, details, search } = require('../controllers/catalog');
+const { about } = require('../controllers/about');
+const { createGet } = require('../controllers/movie');
+const { notFound } = require('../controllers/404');
 
 const router = Router();
 //TODO add routes
-router.get('/', homeController);
+router.get('/', home);
+router.get('/about', about);
+router.get('/details/:id', details);
+router.get('/create', createGet);
+router.get('/search', search);
+
+router.get('*', notFound);
 
 module.exports = {
   router,
